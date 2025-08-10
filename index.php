08d0c342f15334c7f10fc5897f824230e3019590
Kuldeep97164 <jangidkuldeep861@gmail.com>
2025-08-10 09:48:19 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 $get = $_GET['get'];
-$mpdUrl = 'https://linears2-playback.astro.com.my/vsg/41PRoPlPVA72W-bkKRiivWcUpieMdt0C8xZECS4qf0ZCc=W8ZI/dash-wv' . $get;
+$mpdUrl = 'https://linears2-playback.astro.com.my/vsg/41PRoPlPVA72W-bkKRiivWcUpieMdt0C8xZECS4qf0ZCc=W8ZI/dash-wv/jitp-dashisowm/' . $get;
 
 $mpdheads = [
   'http' => [
